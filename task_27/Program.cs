e7667cf9dd6d6d5738ace31434b562b40817d7da
DmitPloh <kroll.88@mail.ru>
2023-09-03 12:13:58 +0300

    finish task27

---
+++ task_27/Program.cs
@@ -0,0 +1,14 @@
+﻿int number = Convert.ToInt32(Console.ReadLine());
+
+int Sum(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum += number % 10;
+        number = number / 10;
+    }
+    return sum;
+}
+
+Console.WriteLine(Sum(number));
