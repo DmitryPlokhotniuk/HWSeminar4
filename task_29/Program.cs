5da8baff0c87bccf5249b9512fad2fd44260e94b
DmitPloh <kroll.88@mail.ru>
2023-09-03 12:24:06 +0300

    finish task29

---
+++ task_29/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] array = new int[8];
+
+void RandomArr (int[] array)
+{
+    int len = array.Length;
+    for (int i = 0; i < len; i++)
+    {
+        array[i] = new Random().Next(0, 255);
+        Console.Write(array[i] + ", ");
+    }
+}
+RandomArr(array);
