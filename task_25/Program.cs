3b22cdb3d03a81caa332ede4891bd57f3a2896ea
DmitPloh <kroll.88@mail.ru>
2023-09-03 11:57:54 +0300

    finish task25

--- task_25/Program.cs
+++ task_25/Program.cs
@@ -1,1 +1,17 @@
 ﻿
+int a = Convert.ToInt32(Console.ReadLine()); // Вводим число
+int b = Convert.ToInt32(Console.ReadLine()); // Вводим значение степени
+    
+int CountPow(int a, int b)
+{
+    int pow = a;
+    for (int i = 1; i < b; i++)
+    {
+        pow *= a;
+    }
+
+    return pow;
+}
+
+Console.WriteLine(CountPow(a, b));   
+Console.WriteLine(Math.Pow(a, b)); // Проверка для себя
